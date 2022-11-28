1b8b0d764d4f277917ab53c7f5eac9173a59d1e8
dzidzi <kopko.mateusz@gmail.com>
2022-11-28 20:51:58 +0100

    add dto

---
+++ src/dto/DriverAttributeDTO.ts
@@ -0,0 +1,48 @@
+import objectHash from 'object-hash';
+
+import {
+  DriverAttribute,
+  DriverAttributeName,
+} from 'src/entity/DriverAttribute';
+
+export class DriverAttributeDTO {
+  private name: DriverAttributeName;
+  private value: string;
+
+  constructor(
+    a:
+      | DriverAttribute
+      | {
+          name: DriverAttributeName;
+          value: string;
+        },
+  ) {
+    if (a instanceof DriverAttribute) {
+      this.name = a.getName();
+      this.value = a.getValue();
+    } else {
+      this.name = a.name;
+      this.value = a.value;
+    }
+  }
+
+  public getName(): DriverAttributeName {
+    return this.name;
+  }
+
+  public setName(name: DriverAttributeName): void {
+    this.name = name;
+  }
+
+  public getValue(): string {
+    return this.value;
+  }
+
+  public setValue(value: string): void {
+    this.value = value;
+  }
+
+  public hashCode(): string {
+    return objectHash({ name: this.name, value: this.value });
+  }
+}
