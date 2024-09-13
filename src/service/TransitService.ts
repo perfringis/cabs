ca7e5f4e05ae8e4c53ef5400bce7b22a5961df57
perfringis <kopko.mateusz@gmail.com>
2024-09-13 17:56:47 +0200

    finish implementation of TransitService

--- src/service/TransitService.ts
+++ src/service/TransitService.ts
@@ -19,6 +19,10 @@ import { CarClass } from 'src/entity/CarType';
 import { Client } from 'src/entity/Client';
 import { NotAcceptableException, NotFoundException } from '@nestjs/common';
 import dayjs from 'dayjs';
+import { DriverPositionDTOV2 } from 'src/dto/DriverPositionDTOV2';
+import { DriverSession } from 'src/entity/DriverSession';
+import { Driver, DriverStatus } from 'src/entity/Driver';
+import { table } from 'console';
 
 export class TransitService {
   constructor(
@@ -280,6 +284,7 @@ export class TransitService {
     return await this.findDriversForTransit(transitId);
   }
 
+  // Abandon hope all ye who enter here...
   public async findDriversForTransit(transitId: string): Promise<Transit> {
     const transit: Transit = await this.transitRepository.getOne(transitId);
 
@@ -297,10 +302,338 @@ export class TransitService {
           distanceToCheck++;
 
           // FIXME: to refactor when the final business logic will be determined
+          if (
+            dayjs(transit.getPublished())
+              .add(300, 'second')
+              .isBefore(dayjs()) ||
+            distanceToCheck >= 20 ||
+            // Should it be here? How is it even possible due to previous status check above loop?
+            transit.getStatus() === Status.CANCELLED
+          ) {
+            transit.setStatus(Status.DRIVER_ASSIGNMENT_FAILED);
+            transit.setDriver(null);
+            transit.setKm(0);
+            transit.setAwaitingDriversResponses(0);
 
-          // TODO: finish it
+            await this.transitRepository.save(transit);
+
+            return transit;
+          }
+
+          let geocoded: number[] = [0, 0];
+
+          try {
+            geocoded = this.geocodingService.geocodeAddress(transit.getFrom());
+          } catch (e) {
+            // Geocoding failed! Ask Jessica or Bryan for some help if needed.
+          }
+
+          const longitude: number = geocoded[1];
+          const latitude: number = geocoded[0];
+
+          //https://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+          //Earth’s radius, sphere
+          //double R = 6378;
+          const R = 6371; // Changed to 6371 due to Copy&Paste pattern from different source
+
+          //offsets in meters
+          const dn: number = distanceToCheck;
+          const de: number = distanceToCheck;
+
+          //Coordinate offsets in radians
+          const dLat: number = dn / R;
+          const dLon: number = de / (R * Math.cos((Math.PI * latitude) / 180));
+
+          //Offset positions, decimal degrees
+          const latitudeMin: number = latitude - (dLat * 180) / Math.PI;
+          const latitudeMax: number = latitude + (dLat * 180) / Math.PI;
+          const longitudeMin: number = longitude - (dLon * 180) / Math.PI;
+          const longitudeMax: number = longitude + (dLon * 180) / Math.PI;
+
+          let driversAvgPositions: DriverPositionDTOV2[] =
+            await this.driverPositionRepository.findAverageDriverPositionSince(
+              latitudeMin,
+              latitudeMax,
+              longitudeMin,
+              longitudeMax,
+              dayjs().subtract(5, 'minute').toDate(),
+            );
+
+          if (!(driversAvgPositions.length === 0)) {
+            const comparator = (
+              d1: DriverPositionDTOV2,
+              d2: DriverPositionDTOV2,
+            ) => {
+              const dist1: number = Math.sqrt(
+                Math.pow(latitude - d1.getLatitude(), 2) +
+                  Math.pow(longitude - d1.getLongitude(), 2),
+              );
+
+              const dist2: number = Math.sqrt(
+                Math.pow(latitude - d2.getLatitude(), 2) +
+                  Math.pow(longitude - d2.getLongitude(), 2),
+              );
+
+              return dist1 - dist2;
+            };
+
+            driversAvgPositions.sort(comparator);
+            driversAvgPositions = driversAvgPositions.splice(0, 20);
+
+            const carClasses: CarClass[] = [];
+            const activeCarClasses: CarClass[] =
+              await this.carTypeService.findActiveCarClasses();
+
+            if (activeCarClasses.length === 0) {
+              return transit;
+            }
+
+            if (transit.getCarType() !== null) {
+              if (activeCarClasses.includes(transit.getCarType())) {
+                carClasses.push(transit.getCarType());
+              } else {
+                return transit;
+              }
+            } else {
+              carClasses.push(...activeCarClasses);
+            }
+
+            const drivers: Driver[] = await Promise.all(
+              driversAvgPositions.map(async (d) => {
+                return d.getDriver();
+              }),
+            );
+
+            const driverSessions: DriverSession[] =
+              await this.driverSessionRepository.findAllByLoggedOutAtNullAndDriverInAndCarClassIn(
+                drivers,
+                carClasses,
+              );
+
+            const activeDriverIdsInSpecificCar: string[] = await Promise.all(
+              driverSessions.map(async (ds) => {
+                return ds.getDriver().getId();
+              }),
+            );
+
+            driversAvgPositions = await Promise.all(
+              driversAvgPositions.filter((dp) => {
+                return activeDriverIdsInSpecificCar.includes(
+                  dp.getDriver().getId(),
+                );
+              }),
+            );
+
+            // Iterate across average driver positions
+            for (const driverAvgPosition of driversAvgPositions) {
+              const driver: Driver = driverAvgPosition.getDriver();
+
+              if (
+                driver.getStatus() === DriverStatus.ACTIVE &&
+                driver.getIsOccupied() === false
+              ) {
+                if (!transit.getDriversRejections().includes(driver)) {
+                  transit.getProposedDrivers().push(driver);
+                  transit.setAwaitingDriversResponses(
+                    transit.getAwaitingDriversResponses() + 1,
+                  );
+                }
+              } else {
+                // Not implemented yet!
+              }
+            }
+
+            await this.transitRepository.save(transit);
+          } else {
+            // Next iteration, no drivers at specified area
+            continue;
+          }
+        }
+      } else {
+        throw new NotAcceptableException('..., id = ' + transitId);
+      }
+    } else {
+      throw new NotFoundException('Transit does not exist, id = ' + transitId);
+    }
+  }
+
+  public async acceptTransit(
+    driverId: string,
+    transitId: string,
+  ): Promise<void> {
+    const driver: Driver = await this.driverRepository.getOne(driverId);
+
+    if (driver === null) {
+      throw new NotFoundException('Driver does not exist, id = ' + driverId);
+    } else {
+      const transit: Transit = await this.transitRepository.getOne(transitId);
+
+      if (transit === null) {
+        throw new NotFoundException(
+          'Transit does not exist, id = ' + transitId,
+        );
+      } else {
+        if (transit.getDriver() !== null) {
+          throw new NotAcceptableException(
+            'Transit already accepted, id = ' + transitId,
+          );
+        } else {
+          if (!transit.getProposedDrivers().includes(driver)) {
+            throw new NotAcceptableException(
+              'Driver out of possible drivers, id = ' + transitId,
+            );
+          } else {
+            if (transit.getProposedDrivers().includes(driver)) {
+              throw new NotAcceptableException(
+                'Driver out of possible drivers, id = ' + transitId,
+              );
+            } else {
+              transit.setDriver(driver);
+              transit.setAwaitingDriversResponses(0);
+              transit.setAcceptedAt(dayjs().toDate());
+              transit.setStatus(Status.TRANSIT_TO_PASSENGER);
+              await this.transitRepository.save(transit);
+
+              driver.setIsOccupied(true);
+              await this.driverRepository.save(driver);
+            }
+          }
         }
       }
     }
+  }
+
+  public async startTransit(
+    driverId: string,
+    transitId: string,
+  ): Promise<void> {
+    const driver: Driver = await this.driverRepository.getOne(driverId);
+
+    if (driver === null) {
+      throw new NotFoundException('Driver does not exist, id = ' + driverId);
+    }
+
+    const transit: Transit = await this.transitRepository.getOne(transitId);
+
+    if (transit === null) {
+      throw new NotFoundException('Transit does not exist, id = ' + transitId);
+    }
+
+    if (!(transit.getStatus() === Status.TRANSIT_TO_PASSENGER)) {
+      throw new NotAcceptableException(
+        'Transit cannot be started, id = ' + transitId,
+      );
+    }
+
+    transit.setStatus(Status.IN_TRANSIT);
+    transit.setStarted(dayjs().toDate());
+
+    await this.transitRepository.save(transit);
+  }
+
+  public async rejectTransit(
+    driverId: string,
+    transitId: string,
+  ): Promise<void> {
+    const driver: Driver = await this.driverRepository.getOne(driverId);
+
+    if (driver === null) {
+      throw new NotFoundException('Driver does not exist, id = ' + driverId);
+    }
+
+    const transit: Transit = await this.transitRepository.getOne(transitId);
+
+    if (transit === null) {
+      throw new NotFoundException('Transit does not exist, id = ' + transitId);
+    }
+
+    transit.getDriversRejections().push(driver);
+    transit.setAwaitingDriversResponses(
+      transit.getAwaitingDriversResponses() - 1,
+    );
+
+    await this.transitRepository.save(transit);
+  }
+
+  public async completeTransit(
+    driverId: string,
+    transitId: string,
+    destinationAddress: AddressDTO,
+  ): Promise<void> {
+    await this._completeTransit(
+      driverId,
+      transitId,
+      destinationAddress.toAddressEntity(),
+    );
+  }
+
+  public async _completeTransit(
+    driverId: string,
+    transitId: string,
+    destinationAddress: Address,
+  ) {
+    destinationAddress = await this.addressRepository.save(destinationAddress);
+
+    const driver: Driver = await this.driverRepository.getOne(driverId);
+
+    if (driver === null) {
+      throw new NotFoundException('Driver does not exist, id = ' + driverId);
+    }
+
+    const transit: Transit = await this.transitRepository.getOne(transitId);
+
+    if (transit === null) {
+      throw new NotFoundException('Transit does not exist, id = ' + transitId);
+    }
+
+    if (transit.getStatus() === Status.IN_TRANSIT) {
+      // FIXME later: add some exceptions handling
+      const geoFrom: number[] = this.geocodingService.geocodeAddress(
+        transit.getFrom(),
+      );
+      const geoTo: number[] = this.geocodingService.geocodeAddress(
+        transit.getTo(),
+      );
+
+      transit.setTo(destinationAddress);
+      transit.setKm(
+        this.distanceCalculator.calculateByMap(
+          geoFrom[0],
+          geoFrom[1],
+          geoTo[0],
+          geoTo[1],
+        ),
+      );
+      transit.setStatus(Status.COMPLETED);
+      transit.calculateFinalCosts();
+      driver.setIsOccupied(false);
+      transit.setCompleteAt(dayjs().toDate());
+      const driverFee: number = await this.driverFeeService.calculateDriverFee(
+        transitId,
+      );
+      transit.setDriversFee(driverFee);
+
+      await this.driverRepository.save(driver);
+
+      await this.awardsService.registerMiles(
+        transit.getClient().getId(),
+        transitId,
+      );
+
+      await this.transitRepository.save(transit);
+
+      await this.invoiceGenerator.generate(
+        transit.getPrice(),
+        transit.getClient().getName() + ' ' + transit.getClient().getLastName(),
+      );
+    } else {
+      throw new NotAcceptableException(
+        'Cannot complete Transit, id = ' + transitId,
+      );
+    }
+  }
+
+  public async loadTransit(id: string): Promise<TransitDTO> {
+    return new TransitDTO(await this.transitRepository.getOne(id));
   }
 }
