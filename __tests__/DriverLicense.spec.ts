d0318f393b900d5fdae94df48f339dfd0be19ce9
perfringis <kopko.mateusz@gmail.com>
2024-10-09 11:38:05 +0200

    add unit test for DriverLicense value object and add implementation of DriverLicense

---
+++ __tests__/DriverLicense.spec.ts
@@ -0,0 +1,33 @@
+import { NotAcceptableException } from '@nestjs/common';
+import { DriverLicense } from 'src/entity/DriverLicense';
+
+describe('DriverLicenseTest', () => {
+  test('cannot create invalid license', () => {
+    // expect
+    expect(() => DriverLicense.withLicense('invalid_license')).toThrow(
+      new NotAcceptableException('Illegal new license no = invalid_license'),
+    );
+
+    expect(() => DriverLicense.withLicense('')).toThrow(
+      new NotAcceptableException('Illegal new license no = '),
+    );
+  });
+
+  test('can create valid license', () => {
+    // when
+    const license: DriverLicense =
+      DriverLicense.withLicense('FARME100165AB5EW');
+
+    // then
+    expect(license.asString()).toEqual('FARME100165AB5EW');
+  });
+
+  test('can create invalid license explicitly', () => {
+    // when
+    const license: DriverLicense =
+      DriverLicense.withoutValidation('invalid_license');
+
+    // then
+    expect(license.asString()).toEqual('invalid_license');
+  });
+});
---
+++ src/entity/DriverLicense.ts
@@ -0,0 +1,31 @@
+import { NotAcceptableException } from '@nestjs/common';
+
+export class DriverLicense {
+  private static readonly DRIVER_LICENSE_REGEX: string =
+    '^[A-Z9]{5}\\d{6}[A-Z9]{2}\\d[A-Z]{2}$';
+
+  private license: string;
+
+  constructor(license: string) {
+    this.license = license;
+  }
+
+  public static withLicense(license: string): DriverLicense {
+    if (
+      license === null ||
+      license.length === 0 ||
+      !license.match(DriverLicense.DRIVER_LICENSE_REGEX)
+    ) {
+      throw new NotAcceptableException('Illegal new license no = ' + license);
+    }
+    return new DriverLicense(license);
+  }
+
+  public static withoutValidation(license: string): DriverLicense {
+    return new DriverLicense(license);
+  }
+
+  public asString(): string {
+    return this.license;
+  }
+}
