6a2f6ab83cde7d28cf57db051b6062b9683f25ad
perfringis <kopko.mateusz@gmail.com>
2024-11-14 19:36:10 +0100

    add unit tests

---
+++ __tests__/ui/Distance.spec.ts
@@ -0,0 +1,42 @@
+import { NotAcceptableException } from '@nestjs/common';
+import { Distance } from 'src/entity/Distance';
+
+describe('DistanceTest', () => {
+  test('cannot understand invalid unit', () => {
+    expect(() => Distance.ofKm(2000).printIn('invalid')).toThrow(
+      new NotAcceptableException('Invalid unit invalid'),
+    );
+  });
+
+  test('can convert to float', () => {
+    expect(Distance.ofKm(2000).toKmInFloat()).toEqual(2000);
+    expect(Distance.ofKm(0).toKmInFloat()).toEqual(0);
+    expect(Distance.ofKm(312.22).toKmInFloat()).toEqual(312.22);
+    expect(Distance.ofKm(2).toKmInFloat()).toEqual(2);
+  });
+
+  test('can represent distance as meters', () => {
+    expect(Distance.ofKm(2000).printIn('m')).toEqual('2,000,000 m');
+    expect(Distance.ofKm(0).printIn('m')).toEqual('0 m');
+    expect(Distance.ofKm(312.22).printIn('m')).toEqual('312,220 m');
+    expect(Distance.ofKm(2).printIn('m')).toEqual('2,000 m');
+  });
+
+  test('can represent distance as km', () => {
+    expect(Distance.ofKm(2000).printIn('km')).toEqual('2,000 km');
+    expect(Distance.ofKm(0).printIn('km')).toEqual('0 km');
+    expect(Distance.ofKm(312.22).printIn('km')).toEqual('312.22 km');
+    expect(Distance.ofKm(312.221111232313).printIn('km')).toEqual('312.221 km');
+    expect(Distance.ofKm(2).printIn('km')).toEqual('2 km');
+  });
+
+  test('can represent distance as miles', () => {
+    expect(Distance.ofKm(2000).printIn('miles')).toEqual('1,242.742 mi');
+    expect(Distance.ofKm(0).printIn('miles')).toEqual('0 mi');
+    expect(Distance.ofKm(312.22).printIn('miles')).toEqual('194.005 mi');
+    expect(Distance.ofKm(312.221111232313).printIn('miles')).toEqual(
+      '194.005 mi',
+    );
+    expect(Distance.ofKm(2).printIn('miles')).toEqual('1.243 mi');
+  });
+});
---
+++ src/entity/Distance.ts
@@ -0,0 +1,62 @@
+import { NotAcceptableException } from '@nestjs/common';
+import objectHash from 'object-hash';
+
+export class Distance {
+  private static readonly MILES_TO_KILOMETERS_RATIO = 1.609344;
+
+  private km: number;
+
+  private constructor(km: number) {
+    this.km = km;
+  }
+
+  public static ofKm(km: number): Distance {
+    return new Distance(km);
+  }
+
+  public toKmInFloat() {
+    return this.km;
+  }
+
+  public hashCode(): string {
+    return objectHash({ km: this.km });
+  }
+
+  public printIn(unit: string) {
+    if (unit === 'km') {
+      if (this.km === Math.ceil(this.km)) {
+        return new Intl.NumberFormat('en-US', {
+          style: 'unit',
+          unit: 'kilometer',
+        }).format(Math.round(this.km));
+      }
+      return new Intl.NumberFormat('en-US', {
+        style: 'unit',
+        unit: 'kilometer',
+      }).format(this.km);
+    }
+
+    if (unit === 'miles') {
+      const distance = this.km / Distance.MILES_TO_KILOMETERS_RATIO;
+      if (distance === Math.ceil(distance)) {
+        return new Intl.NumberFormat('en-US', {
+          style: 'unit',
+          unit: 'mile',
+        }).format(Math.round(distance));
+      }
+      return new Intl.NumberFormat('en-US', {
+        style: 'unit',
+        unit: 'mile',
+      }).format(distance);
+    }
+
+    if (unit === 'm') {
+      return new Intl.NumberFormat('en-US', {
+        style: 'unit',
+        unit: 'meter',
+      }).format(Math.round(this.km * 1000));
+    }
+
+    throw new NotAcceptableException('Invalid unit ' + unit);
+  }
+}
